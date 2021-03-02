2dcafebc1a520defc53375cc99e3c228d62542fd
Seyd <34152761+SeydX@users.noreply.github.com>
2021-03-02 18:39:05 +0100

    Fixed window contact state & other improvements

--- src/accessories/accessory.js
+++ src/accessories/accessory.js
@@ -182,7 +182,7 @@ class CarAccessory {
           error = {
             status: err.response.status,
             message: err.response.statusText
-          }
+          };
         }
       }
       
@@ -205,7 +205,7 @@ class CarAccessory {
   
   async handleBatteryFuel(dataFuel, service){
     
-    let batteryValue;
+    let batteryValue = 100;
     let batteryState = 0;
     
     if(dataFuel.length){
@@ -228,16 +228,12 @@ class CarAccessory {
       
     }
     
-    if(batteryValue !== undefined){
-      
-      if(batteryValue <= 20)
-        batteryState = 1;
+    if(batteryValue <= 20)
+      batteryState = 1;
         
-      service
-        .getCharacteristic(this.api.hap.Characteristic.BatteryLevel)
-        .updateValue(batteryValue);
-        
-    }
+    service
+      .getCharacteristic(this.api.hap.Characteristic.BatteryLevel)
+      .updateValue(batteryValue);
     
     service
       .getCharacteristic(this.api.hap.Characteristic.ChargingState)
@@ -253,7 +249,7 @@ class CarAccessory {
   
   async handleBatteryElectro(dataElectro, service){
     
-    let batteryValue;
+    let batteryValue = 100;
     let batteryState = 0;
     
     if(dataElectro.length){
@@ -276,16 +272,12 @@ class CarAccessory {
       
     }
     
-    if(batteryValue !== undefined){
-      
-      if(batteryValue <= 20)
-        batteryState = 1;
+    if(batteryValue <= 20)
+      batteryState = 1;
         
-      service
-        .getCharacteristic(this.api.hap.Characteristic.BatteryLevel)
-        .updateValue(batteryValue);
-        
-    }
+    service
+      .getCharacteristic(this.api.hap.Characteristic.BatteryLevel)
+      .updateValue(batteryValue);
     
     service
       .getCharacteristic(this.api.hap.Characteristic.ChargingState)
@@ -301,7 +293,7 @@ class CarAccessory {
 
   async handleLock(dataLock, service){
     
-    let lockValue = [];
+    let state = 0;
     
     if(dataLock.length){
     
@@ -309,23 +301,21 @@ class CarAccessory {
         
         if(dataLock[key].doorlockstatusdecklid && (dataLock[key].doorlockstatusdecklid.value === 'false')){
           
-          lockValue.push(1);
+          state = 1;
           
         } else if(dataLock[key].doorlockstatusvehicle && (dataLock[key].doorlockstatusvehicle.value === '1' || dataLock[key].doorlockstatusvehicle.value === '2')){
           
-          lockValue.push(1);
+          state = 1;
           
         } else if(dataLock[key].doorlockstatusgas && (dataLock[key].doorlockstatusgas.value === 'false')){
           
-          lockValue.push(1);
+          state = 1;
           
         }
         
       }
       
     }
-    
-    let state = lockValue.includes(1) ? 1 : 0;
     
     service
       .getCharacteristic(this.api.hap.Characteristic.LockCurrentState)
@@ -341,7 +331,7 @@ class CarAccessory {
   
   async handleDoors(dataVehicle, service){
     
-    let doorValue = [];
+    let state = 0;
     
     if(dataVehicle.length){
     
@@ -351,31 +341,29 @@ class CarAccessory {
         
         if(dataVehicle[key].doorstatusfrontleft && (dataVehicle[key].doorstatusfrontleft.value === 'true')){
         
-          doorValue.push(1);
+          state = 1;
           
         } else if(dataVehicle[key].doorstatusfrontright && (dataVehicle[key].doorstatusfrontright.value === 'true')){
           
-          doorValue.push(1);
+          state = 1;
           
         } else if(dataVehicle[key].doorstatusrearleft && (dataVehicle[key].doorstatusrearleft.value === 'true')){
           
-          doorValue.push(1);
+          state = 1;
           
         } else if(dataVehicle[key].doorstatusrearright && (dataVehicle[key].doorstatusrearright.value === 'true')){
           
-          doorValue.push(1);
+          state = 1;
           
         } else if(dataVehicle[key].decklidstatus && (dataVehicle[key].decklidstatus.value === 'true')){
           
-          doorValue.push(1);
+          state = 1;
           
         }
         
       }
       
     }
-    
-    let state = doorValue.includes(1) ? 1 : 0;
     
     service
       .getCharacteristic(this.api.hap.Characteristic.ContactSensorState)
@@ -387,7 +375,7 @@ class CarAccessory {
   
   async handleWindows(dataVehicle, service){
     
-    let windowValue = [];
+    let state = 0;
     
     if(dataVehicle.length){
     
@@ -395,25 +383,25 @@ class CarAccessory {
         
         //window
         
-        if(dataVehicle[key].windowstatusfrontleft && (dataVehicle[key].windowstatusfrontleft.value === '1')){
+        if(dataVehicle[key].windowstatusfrontleft && dataVehicle[key].windowstatusfrontleft.value !== '2'){
         
-          windowValue.push(1);
+          state = 1;
           
-        } else if(dataVehicle[key].windowstatusfrontright && (dataVehicle[key].windowstatusfrontright.value === '1')){
+        } else if(dataVehicle[key].windowstatusfrontright && dataVehicle[key].windowstatusfrontright.value !== '2'){
           
-          windowValue.push(1);
+          state = 1;
           
-        } else if(dataVehicle[key].windowstatusrearleft && (dataVehicle[key].windowstatusrearleft.value === '1')){
+        } else if(dataVehicle[key].windowstatusrearleft && dataVehicle[key].windowstatusrearleft.value !== '2'){
           
-          windowValue.push(1);
+          state = 1;
           
-        } else if(dataVehicle[key].windowstatusrearright && (dataVehicle[key].windowstatusrearright.value === '1')){
+        } else if(dataVehicle[key].windowstatusrearright && dataVehicle[key].windowstatusrearright.value !== '2'){
           
-          windowValue.push(1);
+          state = 1;
           
-        } else if(dataVehicle[key].sunroofstatus && (dataVehicle[key].sunroofstatus.value !== '0')){
+        } else if(dataVehicle[key].sunroofstatus && dataVehicle[key].sunroofstatus.value !== '0'){
           
-          windowValue.push(1);
+          state = 1;
           
         }
         
@@ -421,8 +409,6 @@ class CarAccessory {
       
     }
     
-    let state = windowValue.includes(1) ? 1 : 0;
-      
     service
       .getCharacteristic(this.api.hap.Characteristic.ContactSensorState)
       .updateValue(state);
@@ -433,7 +419,7 @@ class CarAccessory {
   
   async handleLights(dataVehicle, service){
     
-    let lightValue = [];
+    let state = false;
     
     if(dataVehicle.length){
     
@@ -443,19 +429,19 @@ class CarAccessory {
       
         if(dataVehicle[key].interiorLightsFront && (dataVehicle[key].interiorLightsFront.value === 'true')){
           
-          lightValue.push(1);
+          state = true;
           
         } else if(dataVehicle[key].interiorLightsRear && (dataVehicle[key].interiorLightsRear.value === 'true')){
           
-          lightValue.push(1);
+          state = true;
           
         } else if(dataVehicle[key].readingLampFrontLeft && (dataVehicle[key].readingLampFrontLeft.value === 'true')){
           
-          lightValue.push(1);
+          state = true;
           
         } else if(dataVehicle[key].readingLampFrontRight && (dataVehicle[key].readingLampFrontRight.value === 'true')){
           
-          lightValue.push(1);
+          state = true;
           
         }
         
@@ -463,8 +449,6 @@ class CarAccessory {
       
     }
     
-    let state = lightValue.includes(1) ? true : false;
-      
     service
       .getCharacteristic(this.api.hap.Characteristic.On)
       .updateValue(state);
